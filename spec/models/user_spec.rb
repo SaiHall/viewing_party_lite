11879bef312f8bb633ac1855a0753d2b34fcef42
Sarah Hall <swatson1994@yahoo.com>
2022-07-18 15:15:03 -0400

    Add test for secure password, and add to model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,9 +5,10 @@ RSpec.describe User, type: :model do
     it {should validate_presence_of(:name)}
     it {should validate_presence_of(:email)}
     it {should validate_uniqueness_of(:email)}
+    it {should have_secure_password}
   end
 
   describe 'relationships' do
     it {should have_many :viewing_parties}
-  end 
+  end
 end
