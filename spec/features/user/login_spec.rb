451aa2499efe76dd4dd85d2a4e879756c2651b13
Sarah Hall <swatson1994@yahoo.com>
2022-07-18 19:03:26 -0400

    Test: Include happy path form submission

--- spec/features/user/login_spec.rb
+++ spec/features/user/login_spec.rb
@@ -8,4 +8,15 @@ RSpec.describe 'the login page', type: :feature do
     expect(page).to have_field(:password)
     expect(page).to have_button("Log In")
   end
+
+  it 'can log a user in and redirect them to their dashboard' do
+    user1 = User.create!(name: 'Sai', email: 'SaiLent@overlord.com', password: 'no-u', password_confirmation: 'no-u')
+    visit '/login'
+
+    fill_in :email, with: 'SaiLent@overlord.com'
+    fill_in :password, with: 'no-u'
+    click_button("Log In")
+
+    expect(page).to have_current_path("/users/#{user1.id}")
+  end
 end
